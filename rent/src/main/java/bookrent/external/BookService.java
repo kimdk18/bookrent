817160a4ab7909a219ee74399a07240edf4e61c5
kimdk18 <kongkai2@gamil.com>
2021-07-03 20:58:41 +0000

    modify about config map

--- rent/src/main/java/bookrent/external/BookService.java
+++ rent/src/main/java/bookrent/external/BookService.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@FeignClient(name="book", url="http://${api.url.book}:8080", fallback=BookServiceFallback.class)
-@FeignClient(name="book", url="http://localhost:8081", fallback=BookServiceFallback.class)
 //@FeignClient(name="book", url="http://localhost:8081")
+//@FeignClient(name="book", url="http://localhost:8081", fallback=BookServiceFallback.class)
+@FeignClient(name="book", url="http://${api.url.book}:8080", fallback=BookServiceFallback.class)
 public interface BookService {
     @RequestMapping(method= RequestMethod.GET, path="/books/checkAndChangeStatus")
     public boolean checkAndChangeStatus(@RequestParam("bookId") Long bookId);
